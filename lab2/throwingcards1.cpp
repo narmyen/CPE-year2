00f0a55e0d95f089fa4415b207817d52f4aa7233
narmyen <tipananchakreerat@gmail.com>
2023-12-17 21:00:16 +0700

    add throwingcards and test file

--- lab2/throwingcards1.cpp
+++ lab2/throwingcards1.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
-#include <bits/stdc++.h>
-#include <list>
-#include <string>
+#include <queue>
 using namespace std;
 
-int main(){
+int main() {
+    queue<int> q;
+    int n, x;
 
-  
+    string output;
 
-  return 1;
+    while (cin >> n, n) {
+        for (int i = 1; i <= n; i++) {
+            q.push(i);
+        }
+
+        output += "Discarded cards: ";
+        while (q.size() > 1) {
+            output += to_string(q.front());
+            q.pop();
+            x = q.front();
+            q.push(x);
+            q.pop();
+            if (q.size() == 1) {
+                output += "\n";
+            } else {
+                output += ", ";
+            }
+        }
+        output += "Remaining card: " + to_string(q.front()) + "\n";
+        q.pop();
+    }
+
+    cout << output;
+
+    return 0;
 }
